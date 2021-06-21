8d3ce6aec7667877cfc19ea9f30d9b590778731d
luuk <luuk_knippenberg@hotmail.com>
2021-06-21 13:13:16 +0200

    Added planned lessons for normal user
    - Updated dependencies
    - Fixed hasEditPermission bug

--- philips_frontend/src/components/Dashboard/PlannedLessons.vue
+++ philips_frontend/src/components/Dashboard/PlannedLessons.vue
@@ -6,18 +6,21 @@
     class="elevation-1"
     style="width: 100%"
   >
-        <template v-slot:top>
-          <v-toolbar flat>
-            <v-toolbar-title>Lessons</v-toolbar-title>
-          </v-toolbar>
-        </template>
-        
-        <template v-slot:item.actions="{ item }" v-if="HasEditPermission">
-          <v-icon small class="mr-2" @click="openEditDialog(item)">
-            mdi-pencil
-          </v-icon>
-          <v-icon small @click="openDeleteDialog(item)"> mdi-delete </v-icon>
-        </template></v-data-table>
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>Planned Lessons</v-toolbar-title>
+      </v-toolbar>
+    </template>
+    
+    <template v-slot:item.actions="{ item }" >
+      <v-icon v-if="HasEditPermission" small class="mr-2" @click="openEditDialog(item)">
+        mdi-pencil
+      </v-icon>
+      <v-icon v-if="HasEditPermission" small @click="openDeleteDialog(item)"> mdi-delete </v-icon>
+      
+      <router-link :to="'Game?id=' + item.lesson.id"  :key="item.id"><v-icon mall > mdi-chevron-right </v-icon></router-link>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
@@ -26,21 +29,24 @@ export default {
   props:{
     HasEditPermission: Boolean,
   },
-    created() {
+  created() {
+    
     const config = {
       method: "get",
       url: "/Lesson/GetPlanned/"+ store.state.user.id ,
     };
+
     this.$axios(config)
       .then((result) => {
-        this.Lessons = result.data;
+        this.lessons = result.data;
         this.loading = false;
       })
       .catch((error) => {
         this.error = true;
         this.loading = false;
         console.log(error);
-      });
+    });
+
   },
   data: () =>({
       lessons:[],
@@ -50,7 +56,12 @@ export default {
       {
         text: "Name",
         align: "start",
-        value: "name",
+        value: "lesson.name",
+      },
+      {
+        text: "Planned Time",
+        align: "start",
+        value: "plannedTime",
       },
       {
         text: "Actions",
